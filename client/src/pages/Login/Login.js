16508d3cf7a1f441c423a28be509ceffd7f54418
Kawasaki <iduchungho@gmail.com>
2022-09-16 13:48:08 +0700

    update login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -6,6 +6,45 @@ import {
 
 import React from 'react';
 import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/Form';
+
+export function loginSite(){
+    return (
+        <div className='login-wallpaper'>
+            <div className='login-container'>
+                <div className='login-logo'>
+                    <FontAwesomeIcon icon={faTruckFast} className='login-logo-icon'></FontAwesomeIcon>
+                    <div>
+                        URBAN WASTE<br />
+                        COLLECTION
+                        AID - UWC 2.0
+                    </div>
+                </div>
+                <div className='login-divider'></div>
+                <div className='login-body'>
+                    <Form>
+                        <Form.Group controlId='formBasicEmail'>
+                            <Form.Label>Email or Username</Form.Label>
+                            <Form.Control type='email' placeholder='Enter email or username' />
+                            <Form.Text>We'll never share your email with anyone else</Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId='formBasicPassword'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' />
+                        </Form.Group>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='Check me out' />
+                        </Form.Group>
+
+                        <Button type='submit'>Login</Button>
+
+                    </Form>
+                </div>
+                <div className='login-divider'></div>
+            </div>
+        </div>
+    )
+}
 
 function Login() {
     return (
--- client/src/pages/Login/LoginSite.js
+++ client/src/pages/Login/LoginSite.js
@@ -1,50 +1,47 @@
+import './Login.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faTruckFast
+} from '@fortawesome/free-solid-svg-icons';
+
+import React from 'react';
+import Button from 'react-bootstrap/esm/Button';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
-
-import './LoginSite.css'
 
 function LoginSite() {
     return (
-        <div className='backgroud-site'>
-            <div className='login-form-container'>
-
-                <div className='login-logo-container'>
-                    login logo container navbar
-
-                </div>
-
-
-                <div className='login'>
-                    <div className='login-form'>
-                        <div className='header-login-form'>
-
-                            Login
-                        </div>
-                        <div className='body-login-form'>
-                            <Form>
-                                <Form.Group controlId='formBasicEmail'>
-                                    <Form.Label>Email or Username</Form.Label>
-                                    <Form.Control type = 'email' placeholder = 'Enter email or username'/>
-                                    <Form.Text>We'll never share your email with anyone else</Form.Text>
-                                </Form.Group>
-                                <Form.Group controlId ='formBasicPassword'>
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type = 'password'/>
-                                </Form.Group>
-                                <Form.Group controlId ='formBasicCheckbox'>
-                                    <Form.Check type = 'checkbox' label = 'Check me out'/>
-                                </Form.Group>
-
-                                <Button type = 'submit'>Login</Button>
-
-                            </Form>
-                        </div>
-                    </div>
-                    <div className='login-backgroud'>
-                        
+        <div className='login-wallpaper'>
+            <div className='login-container'>
+                <div className='login-logo'>
+                    <FontAwesomeIcon icon={faTruckFast} className='login-logo-icon'></FontAwesomeIcon>
+                    <div>
+                        URBAN WASTE<br />
+                        COLLECTION
+                        AID - UWC 2.0
                     </div>
                 </div>
+                <div className='login-divider'></div>
+                <div className='login-body'>
+                    <Form>
+                        <Form.Group controlId='formBasicEmail' className='form-group'>
+                            <Form.Label>Email or Username</Form.Label>
+                            <Form.Control type='email' placeholder='Enter email or username' />
+                            <Form.Text>We'll never share your email with anyone else</Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId='formBasicPassword' className='form-group'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' />
+                        </Form.Group>
+                        <Form.Group controlId='formBasicCheckbox' className='form-group'>
+                            <Form.Check type='checkbox' label='Check me out' />
+                        </Form.Group>
+
+                        <Button type='submit' className='login-form-btn' >Login</Button>
+
+                    </Form>
+                </div>
+                <div className='login-divider'></div>
             </div>
         </div>
     )
